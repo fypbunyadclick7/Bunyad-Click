d2e1bf91badfa4feaa5ab179f784987fb5fe59b7
Subhan-Anjum <subhananjum001@gmail.com>
2025-02-05 15:43:15 +0500

    manage seller profile, view sellerProfile, implement cloudinaryservice
    manage seller profile, view seller Profile, implement cloudinary-service

---
+++ src/Pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BuyerNavbar from '../Components/buyernavbar'
+
+export default function home() {
+  return (
+    <div>
+        <BuyerNavbar />
+      
+    </div>
+  )
+}
